cf1413c7260bd140be091598c83f195008f9e8e8
chicken <hibary.taiwan@gmail.com>
2023-08-24 22:16:36 +0800

    feat: controller

---
+++ src/main/java/com/example/demo/demoController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoController {
+
+    @RequestMapping("/Hi")
+    public String index() {
+        return "Hi HI";
+    }
+}
